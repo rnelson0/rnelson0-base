aa2db1fea8a7bddbf356f57dd67d35efeb753c27
Nelson, Robert <rnelson0@gmail.com>
2014-05-07 01:35:36 +0000

    Update SSH config to work with new version of saz

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,8 +39,30 @@ class base {
 
   include ::motd
 
-  include ::ssh
-  ::ssh::server::configline { 'PermitRootLogin': value => 'yes' }
+  # SSH server and client
+  class { '::ssh::server':
+    options => {
+      'PermitRootLogin' => 'yes',
+      'Protocol' => '2',
+      'SyslogFacility' => 'AUTHPRIV',
+      'PasswordAuthentication' => 'yes',
+      'GSSAPIAuthentication' => 'yes',
+      'GSSAPICleanupCredentials' => 'yes',
+      'AcceptEnv' => 'LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT LC_IDENTIFICATION LC_ALL LANGUAGE XMODIFIERS',
+      'Subsystem' => '      sftp    /usr/libexec/openssh/sftp-server',
+      'Banner' => '/etc/issue.net',
+    },
+  }
+  class { '::ssh::client':
+    options => {
+      'Host *' => {
+        'SendEnv'                   => 'LANG LC_*',
+        'HashKnownHosts'            => 'yes',
+        'GSSAPIAuthentication'      => 'yes',
+        'GSSAPIDelegateCredentials' => 'no',
+      },
+    },
+  }
 
   class { '::ntp':
     servers => [ '0.pool.ntp.org', '2.centos.pool.ntp.org', '1.rhel.pool.ntp.org'],
